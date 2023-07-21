99e3e28d0faa0df6ac04e5aea71c1a5598869e6c
MJ Rossetti <s2t2@users.noreply.github.com>
2023-07-21 16:57:09 -0400

    Refactor via inheritance

--- app/reduction/pca_tuner.py
+++ app/reduction/pca_tuner.py
@@ -1,68 +1,59 @@
 
 import os
 
-from pandas import DataFrame
 import plotly.express as px
 
 from app import RESULTS_DIRPATH
 from app.reduction.pipeline import ReductionPipeline, FIG_SAVE, FIG_SHOW
+from app.reduction.tuner import ReductionTuner, MAX_COMPONENTS
 
 
-MAX_COMPONENTS = os.getenv("MAX_COMPONENTS")
+class PCATuner(ReductionTuner):
 
+    def __init__(self, df, label_cols, results_dirpath=RESULTS_DIRPATH, max_components=MAX_COMPONENTS):
+        super().__init__(df=df, label_cols=label_cols, results_dirpath=results_dirpath, max_components=max_components,
+                         reducer_type="PCA")
 
-class PCATuner:
+    #def perform(self):
+    #    self.results = []
+    #
+    #    # if we have lots of columns / features, we might want to abbreviate the search space and override with a max value, otherwise search over all available features
+    #    max_components = self.max_components or len(self.feature_names)
+    #    # get the explained variance for each n up to the max number of components to search over
+    #    for n_components in range(1, max_components+1):
+    #
+    #        pipeline = ReductionPipeline(df=self.df, label_cols=self.label_cols,
+    #                                     reducer_type="PCA", n_components=n_components)
+    #        pipeline.perform()
+    #
+    #        pca = pipeline.reducer
+    #        self.results.append({
+    #            "n_components": n_components,
+    #            "explained_variance": pca.explained_variance_ratio_.sum(),
+    #            "eigenvals": pca.explained_variance_, # number of vals depend on n components
+    #            #"loadings": loadings,
+    #            #"embeddings": embeddings
+    #        })
+    #    self.results_df = DataFrame(self.results)
+    #    print(self.results_df[["n_components", "explained_variance"]].head())
 
-    def __init__(self, df, label_cols=[], results_dirpath=RESULTS_DIRPATH, max_components=MAX_COMPONENTS):
-        self.df = df
-
-        self.label_cols = label_cols
-        self.feature_names = self.df.drop(columns=self.label_cols).columns.tolist()
-
-        if max_components:
-            max_components = int(max_components)
-        self.max_components = max_components
-
-        self.results_dirpath = results_dirpath
-        #os.makedirs(self.results_dirpath, exist_ok=True)
-
-        self.results = None
-        self.results_df = None
-
-
-    def perform(self):
-        self.results = []
-
-        # if we have lots of columns / features, we might want to abbreviate the search space and override with a max value, otherwise search over all available features
-        max_components = self.max_components or len(self.feature_names)
-        # get the explained variance for each n up to the max number of components to search over
-        for n_components in range(1, max_components+1):
-            # we need to use PCA specifically because unlike other methods it gives us the explainability metrics
-            pipeline = ReductionPipeline(df=self.df, label_cols=self.label_cols,
-                                         reducer_type="PCA", n_components=n_components)
-            pipeline.perform()
-
-            pca = pipeline.reducer
-            self.results.append({
+    def collect_result(self, pipeline:ReductionPipeline, n_components:int):
+        pca = pipeline.reducer
+        self.results.append({
                 "n_components": n_components,
                 "explained_variance": pca.explained_variance_ratio_.sum(),
                 "eigenvals": pca.explained_variance_, # number of vals depend on n components
                 #"loadings": loadings,
                 #"embeddings": embeddings
             })
-        self.results_df = DataFrame(self.results)
+
+    def print_results(self):
         print(self.results_df[["n_components", "explained_variance"]].head())
-
-
-
-
-
-
 
     def plot_explained_variance(self, height=500, fig_show=FIG_SHOW, fig_save=FIG_SAVE, subtitle=None, results_dirpath=None):
         title = f"Total Explained Variance by Number of Components (PCA)"
-        if subtitle:
-            title += f"<br><sup>{subtitle}</sup>"
+        subtitle = subtitle or f"Max Components: {self.max_components}"
+        title += f"<br><sup>{subtitle}</sup>"
 
         fig = px.line(self.results_df, x="n_components", y="explained_variance",
                 title=title, height=height,
@@ -73,8 +64,11 @@ class PCATuner:
 
         if fig_save:
             results_dirpath = results_dirpath or self.results_dirpath
-            image_filepath = os.path.join(results_dirpath, "pca-explained-variance.png")
+            filestem = "pca-explained-variance" if not self.max_components else f"pca-explained-variance-{self.max_components}"
+            image_filepath = os.path.join(results_dirpath, f"{filestem}.png")
+            html_filepath = os.path.join(results_dirpath, f"{filestem}.html")
             fig.write_image(image_filepath)
+            fig.write_html(html_filepath)
         #return fig
 
 
@@ -86,6 +80,7 @@ class PCATuner:
         print("COMPONENT NUMBERS:", component_numbers)
 
         title=f"Scree Plot of Eigenvalues by Component (PCA)"
+        subtitle = subtitle or f"Max Components: {self.max_components}"
         if subtitle:
             title += f"<br><sup>{subtitle}</sup>"
 
@@ -99,8 +94,11 @@ class PCATuner:
 
         if fig_save:
             results_dirpath = results_dirpath or self.results_dirpath
-            image_filepath = os.path.join(results_dirpath, "pca-scree.png")
+            filestem = "pca-scree" if not self.max_components else f"pca-scree-{self.max_components}"
+            image_filepath = os.path.join(results_dirpath, f"{filestem}.png")
+            html_filepath = os.path.join(results_dirpath, f"{filestem}.html")
             fig.write_image(image_filepath)
+            fig.write_html(html_filepath)
         #return fig
 
 
