f9b802e6059867f5d3ea8281d7d56a1ba2c411b2
MJ Rossetti <s2t2@users.noreply.github.com>
2023-07-24 01:55:59 -0400

    Refactor

--- app/clustering/kmodes.py
+++ app/clustering/kmodes.py
@@ -1,55 +1,60 @@
+
 import os
 
 # https://github.com/nicodv/kmodes
 from kmodes.kmodes import KModes
-#from pandas import DataFrame
-#from plotly.express import scatter
 
-from app.clustering import N_CLUSTERS, CLUSTERING_RESULTS_DIRPATH, write_results_json, clustering_metrics
+from app.clustering import ClusteringPipeline, CLUSTERING_RESULTS_DIRPATH
 
+N_CLUSTERS = int(os.getenv("N_CLUSTERS", default="2"))
 N_ITER = int(os.getenv("N_ITER", default="10"))
+
+
+class KModesPipeline(ClusteringPipeline):
+
+    def __init__(self, ds=None, x_scale=True, n_clusters=N_CLUSTERS, n_iter=N_ITER):
+        super().__init__(ds=ds, x_scale=x_scale)
+
+        self.n_clusters = n_clusters
+        self.n_iter = n_iter
+        self.results_filestem = os.path.join(CLUSTERING_RESULTS_DIRPATH, f"kmodes_{self.n_clusters}_clusters_{self.n_iter}_iter")
+
+        # init : {'Huang', 'Cao', 'random' or an ndarray}, default: 'Cao'
+        #     Method for initialization:
+        #     'Huang': Method in Huang [1997, 1998]
+        #     'Cao': Method in Cao et al. [2009]
+        #     'random': choose 'n_clusters' observations (rows) at random from data for the initial centroids.
+        #
+        # n_jobs : If 1, no parallel. If -1 all CPUs are used
+        #
+        # n_init : Number of time the k-modes algorithm will be run with different centroid seeds.
+        # ... The final results will be the best output of n_init consecutive runs in terms of cost.
+        # ... default=10
+
+        self.model = KModes(n_clusters=n_clusters, init='Huang', n_init=n_iter, verbose=1, n_jobs=-1, random_state=99)
+
+
+    @property
+    def results(self):
+        kmodes_params = self.model.get_params()
+        del kmodes_params['cat_dissim'] # this is a function and not serializable for JSON storage
+
+        kmodes_results = {
+            "model_params": kmodes_params,
+            "kmodes_cost": self.model.cost_
+        }
+        return {**super().results, **kmodes_results}
+
+
+
+
 
 
 if __name__ == "__main__":
 
-    from app.dataset import Dataset
 
-    ds = Dataset()
-    x = ds.x_scaled
-
-    n_clusters = N_CLUSTERS
-    n_iter = N_ITER
-    results_filestem = os.path.join(CLUSTERING_RESULTS_DIRPATH, f"kmodes_{n_clusters}_clusters_{n_iter}_iter")
-
-    # init : {'Huang', 'Cao', 'random' or an ndarray}, default: 'Cao'
-    #     Method for initialization:
-    #     'Huang': Method in Huang [1997, 1998]
-    #     'Cao': Method in Cao et al. [2009]
-    #     'random': choose 'n_clusters' observations (rows) at random from data for the initial centroids.
-    #
-    # n_jobs : If 1, no parallel. If -1 all CPUs are used
-    #
-    # n_init : Number of time the k-modes algorithm will be run with different centroid seeds.
-    # ... The final results will be the best output of n_init consecutive runs in terms of cost.
-    # ... default=10
-
-    km = KModes(n_clusters=n_clusters, init='Huang', n_init=n_iter, verbose=1, n_jobs=-1, random_state=99)
-
-    km.fit_predict(x)
-    labels = km.labels_
-
-    labels_df = ds.labels_slim
-    labels_df[f"kmodes_{n_clusters}_label"] = labels
-    labels_df.to_csv(f"{results_filestem}.csv", index=False)
-
-    km_params = km.get_params()
-    del km_params['cat_dissim'] # this is a function and not serializable for JSON storage
-    result = {
-        "kmodes_params": km_params,
-        "kmodes_cost": km.cost_
-    }
-    result = clustering_metrics(x=x, cluster_labels=labels, labels_df=labels_df, result=result)
-    write_results_json(result=result, json_filepath=f"{results_filestem}.json")
+    pipeline = KModesPipeline()
+    pipeline.perform()
 
 
     ## km.cluster_centroids_.shape #> (2, 1536) row per cluster, column per feature
