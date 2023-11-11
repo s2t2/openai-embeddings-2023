115f766c7d305d42471a80153064af027b51d315
MJ Rossetti <s2t2@users.noreply.github.com>
2023-11-10 21:26:52 -0500

    De-couple service class responsibilities

--- app/model_storage.py
+++ app/model_storage.py
@@ -13,20 +13,15 @@ PROJECT_ID = os.getenv("GOOGLE_PROJECT_NAME") # "my-project"
 BUCKET_NAME = os.getenv("BUCKET_NAME") # "my-bucket" needs to be globally unique!
 
 
-class ModelStorage:
-
-    def __init__(self, model_dirname:str, project_id=PROJECT_ID, bucket_name=BUCKET_NAME):
+class StorageService:
+    def __init__(self, project_id=PROJECT_ID, bucket_name=BUCKET_NAME):
         """ Params model_dirname like 'my-model' """
         self.project_id = project_id
         self.bucket_name = bucket_name
+        print("-----------------------")
+        print("CLOUD STORAGE SERVICE...")
         print("PROJECT ID:", self.project_id)
         print("BUCKET NAME:", self.bucket_name)
-
-        self.model_dirname = model_dirname
-        self.model_filepath = os.path.join(self.model_dirname, "model.joblib")
-        self.local_model_filepath = self.model_filepath
-        self.hosted_model_filepath = self.model_filepath
-        print("MODEL DIRNAME:", self.model_dirname)
 
 
     @property
@@ -50,19 +45,37 @@ class ModelStorage:
     def bucket(self):
         return self.find_or_create_bucket()
 
+
+
+
+class ModelStorage(StorageService):
+
+    def __init__(self, model_dirname:str, project_id=PROJECT_ID, bucket_name=BUCKET_NAME):
+        """ Params model_dirname like 'my-model' """
+        super().__init(project_id=project_id, bucket_name=bucket_name)
+
+        self.model_dirname = model_dirname
+        self.model_filepath = os.path.join(self.model_dirname, "model.joblib") # needs to be called 'model.joblib' specifically, for hosting from cloud storage on Google Vertex AI
+        self.local_model_filepath = self.model_filepath
+        self.hosted_model_filepath = self.model_filepath
+        print("MODEL DIRNAME:", self.model_dirname)
+
     @property
     def model_blob(self):
         return self.bucket.blob(self.hosted_model_filepath)
 
-    def upload_model(self, model):
-        print("DUMPING MODEL...")
+    def save_model(self, model):
+        print("SAVING MODEL (LOCAL)...")
         os.makedirs(self.model_dirname, exist_ok=True)
         joblib.dump(model, self.local_model_filepath)
-        print("UPLOADING MODEL...")
-        self.upload_model_from_file()
 
     def upload_model_from_file(self):
+        print("UPLOADING MODEL...")
         self.model_blob.upload_from_filename(self.local_model_filepath)
+
+    def save_and_upload_model(self, model):
+        self.save_model(model)
+        self.upload_model_from_file()
 
     def download_model(self):
         print("DOWNLOADING MODEL...")
@@ -70,10 +83,12 @@ class ModelStorage:
             return joblib.load(file)
 
 
+
+
 if __name__ == "__main__":
 
 
-    storage = ModelStorage("example-model")
+    storage = StorageService()
 
     print("---------------------")
     for bucket in storage.buckets:
